530914f2cf1c6acba4341fbe7240afaf4b8c06f8
GPep <glenn@darkmink.com>
2018-10-14 17:08:32 +0100

    Draft Functions

---
+++ Get-SQLServerVersion.ps1
@@ -0,0 +1,34 @@
+﻿function Get-SQLServerVersion
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$ComputerName
+	)
+	process
+	{
+		try
+		{
+
+            [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | out-null
+            $srv = New-Object "Microsoft.SqlServer.Management.Smo.Server" $ComputerName
+   			if (-not $srv.version)
+			{
+				throw 'Cannot Connect to Server'
+			}
+            
+
+		    else
+			{
+            $version = $srv.Version
+            return $version
+			}
+		}
+		catch
+		{
+			Write-Error -Message $_.Exception.Message
+		}
+	}
+}
