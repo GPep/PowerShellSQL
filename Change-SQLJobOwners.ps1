4191af61a8f0a1f2df74240e441c3f9f4baf3e63
Glenn Pepper <glenn@darkmink.com>
2018-10-31 13:39:57 +0000

    version1

---
+++ Change-SQLJobOwners.ps1
@@ -0,0 +1,128 @@
+﻿Function Change-SQLJobOwners {
+<#
+	.SYNOPSIS
+    This function can change the SQL Agent Job owner for all jobs on a server. using the -whatif parameter simply creates a text file with the SQL script
+		
+
+    .EXAMPLE
+    Change-SQLJobOwners -Server 'ServerName' -NewOwner 'SA'
+
+    This will change all job owners to 'SA' where the owner is set to something else. An output file is saved to C:\Temp\servername - changeJobOwners.txt if the server has a list of jobs that need the owner changed.
+
+    .PARAMETERS
+    -Server 
+    This is the server/instance name
+    -NewOwner
+    This is the login that you wish to be the new owner
+    -Whatif 
+    This parameter will not update the jobs. It will simply create the text file with the SQL script in it.
+
+
+
+#>
+
+
+
+    Param(
+          [Parameter(Mandatory)]
+          [string[]]$servers,
+          [Parameter(Mandatory=$false)]
+          [ValidateLength(0,80)]
+          [string]$newOwner='SA',
+          [Parameter(Mandatory=$false)]
+		  [ValidateNotNullOrEmpty()]
+		  [switch]$WhatIf=$false
+    )
+    Process
+    {
+
+
+        try 
+        {
+
+
+        Foreach ($server in $servers)
+        {
+
+        $nl = [Environment]::NewLine
+    
+        $srv = New-Object Microsoft.SqlServer.Management.Smo.Server($server)
+
+        $ServerName = $server.replace('\','-')
+
+        $logFile = "C:\Temp\$serverName - ChangeJobOwners.txt"
+        IF (!(Test-Path $logFile))
+        {
+        new-item $logFile
+        }
+        else
+        {
+        Clear-Content -Path $logFile
+        }
+
+        foreach ($job in $srv.JobServer.Jobs)
+        
+        {
+            if ($job.OwnerLoginName -ne $newOwner)
+            {
+
+                #Check New Login exists
+
+                $ServerName = $server.replace('\','-')
+
+
+                $dbs=$srv.Logins
+                if($dbs.contains($newOwner))
+                {
+                "$newOwner found" | out-file -FilePath $logFile -append
+                $Exists = $true
+                }
+
+                else
+                {
+                "$NewOwner Not Found therefore the update will not run" | out-file -FilePath $logFile -append
+                $Exists = $false
+                
+                }
+                
+                If ($Exists -eq $true)
+                {
+                $sqlquery = "---- Current owner: " + $job.OwnerLoginName + $nl + "EXEC msdb.dbo.sp_update_job @job_id=N'" + $job.JobID + "'" + $nl + ", @owner_login_name=N'$newOwner'" 
+                $sqlquery | out-file -filepath $logFile -append
+                
+                If ($whatif -eq $false)
+                {
+                Invoke-Sqlcmd -ServerInstance $server -query $sqlquery
+                }
+
+                }
+
+            }
+
+        }
+
+
+        }
+
+        }
+        Catch {
+
+            $ErrorMessage = $_.Exception.Message
+            Write-Error -Message $ErrorMessage
+            $ErrorMessage| out-file $logFile -append
+              }
+        finally 
+        {
+        
+        <#
+        foreach ($server in $servers)
+        {
+        $ServerName = $server.replace('\','-')
+        get-item "C:\Temp\$ServerName - ChangeJobOwners.txt" | where {-not(select-string -path $_.FullName -pattern "EXEC msdb.dbo.sp_update_job @job_id=")} | remove-item
+
+        }
+        #>
+        }
+    }
+
+}
---
+++ Test-SQLInstance.ps1
@@ -0,0 +1,53 @@
+﻿Function Test-SQLInstance 
+{
+<#
+	.SYNOPSIS
+    This functions tests your SQL Connections and confirms the last Service Restart. It can query multiple servers at once.
+		
+
+    .EXAMPLE
+
+
+
+    .PARAMETERS
+
+
+
+
+#>
+[CmdletBinding()]
+	param
+	(
+		[Parameter]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$ComputerName
+    )
+    $return = @()
+    $SQL = "SELECT @@SERVERNAME as name, Create_Date FROM sys.databases WHERE name = 'TempDB'"
+    foreach ($computer in $ComputerName)
+    {
+    try
+    {
+    $row = NeW-Object -TypeName PsObject -Property @{'InstanceName'=$computer; "StartUpTime"=$null}
+    $Check=Invoke-Sqlcmd -ServerInstance $computer -Database TempDB -Query $SQL -ErrorAction stop -connect
+    $row.InstanceName = $Check.Name
+    $row.StartupTime = $check.Create_Date
+    }
+
+    catch 
+    {
+
+    
+    
+    
+    }
+    Finally
+    {
+
+    $return += $row 
+    }
+}
+
+
+
+}
